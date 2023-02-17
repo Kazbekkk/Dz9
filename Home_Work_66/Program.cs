dd438045db219d44f79ab6cae4a4ac15dda9aa11
Kazbek <bender.rodrigiz2014@gmail.com>
2023-02-17 14:32:32 +0300

    Сумма натуральных чисел

---
+++ Home_Work_66/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Задача 66: Задайте значения M и N.
+Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+ */
+Console.Clear();
+
+int min, max, cons;
+
+Console.WriteLine("Программа находит сумму натуральных элементов в промежутке от min до max.");
+Console.Write("Введите число min = ");
+min = int.Parse(Console.ReadLine());
+cons = min;
+
+Console.Write("Введите число max = ");
+max = int.Parse(Console.ReadLine());
+
+
+int num = Summ(min, max, cons);
+
+int Summ(int M, int N, int Const)
+{
+    int num = Const;
+    if (N > num)
+    {
+        Summ(M + N, N - 1, Const);
+    }
+    else if (N == num)
+    {
+        Console.WriteLine(M);
+    }
+    return M;
+}
